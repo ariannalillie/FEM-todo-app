2e2d0441cfe64419eadc338c87afce7626f5345d
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-20 14:54:41 -0400

    create todo list items state and handle entering todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setTodos([...todos, e.target.value]);
+    }
+  };
+
   return (
     <div className="todo-list">
-      To Do List
+      <input type="text" onKeyDown={(e) => handleKeyDown(e)} />
+      {todos.map((todo, idx) =>  {
+        return <div key={idx}>{todo}</div>;
+      })}
     </div>
   );
 }
