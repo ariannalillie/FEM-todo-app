e79d7dd6f2cacfe82b4081ec9abbd61656612d4a
Arianna Johnson <ariannajohnson588@gmail.com>
2022-09-21 09:23:08 -0400

    handle theme toggle function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { useState } from "react";
-import checkIcon from './images/icon-check.svg';
+import checkIcon from "./images/icon-check.svg";
+import moon from "./images/icon-moon.svg";
+import sun from "./images/icon-sun.svg";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [isChecked, setIsChecked] = useState(false);
+  const [isDark, setIsDark] = useState(false);
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
@@ -16,12 +19,24 @@ function App() {
   // Handle checkbox click
   const handleCheck = () => {
     setIsChecked(!isChecked);
-  }
+  };
+
+  // Handle toggling between dark and light mode
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
 
   return (
     <div>
       <div className="todo-list-container">
-      <h1 className="header">TODO</h1>
+        <div className="top-container">
+          <h1 className="header">TODO</h1>
+          <img
+            src={isDark ? sun : moon}
+            alt="theme toggle"
+            onClick={toggleTheme}
+          />
+        </div>
         <input
           className="input"
           type="text"
@@ -32,10 +47,13 @@ function App() {
           return (
             <>
               <div key={idx} className="list-item">
-                <div className={isChecked ? 'checked' : "checkbox"} onClick={handleCheck}>
+                <div
+                  className={isChecked ? "checked" : "checkbox"}
+                  onClick={handleCheck}
+                >
                   {isChecked && <img src={checkIcon} alt="check icon" />}
                 </div>
-                <p className={isChecked ? 'checked-todo' : ""}>{todo}</p>
+                <p className={isChecked ? "checked-todo" : ""}>{todo}</p>
               </div>
             </>
           );
